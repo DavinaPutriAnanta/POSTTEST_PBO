159e802803dd6458156c4aa287e2d2f17e2a4d4a
DavinaPutriAnanta <162094731+DavinaPutriAnanta@users.noreply.github.com>
2025-03-23 18:22:01 +0800

    Add files via upload

---
+++ POSTTEST2/scentique/src/kelas/Admin.java
@@ -0,0 +1,13 @@
+package kelas;
+
+
+class Admin {
+    String idAdmin, nama, email, password;
+    
+    public Admin(String idAdmin, String nama, String email, String password) {
+        this.idAdmin = idAdmin;
+        this.nama = nama;
+        this.email = email;
+        this.password = password;
+    }
+}
---
+++ POSTTEST2/scentique/src/kelas/CustomParfum.java
@@ -0,0 +1,15 @@
+package kelas;
+
+
+class CustomParfum {
+    String idCustom, idPelanggan, kelasParfum, notesParfum;
+    double hargaCustom;
+    
+    public CustomParfum(String idCustom, String idPelanggan, String kelasParfum, String notesParfum, double hargaCustom) {
+        this.idCustom = idCustom;
+        this.idPelanggan = idPelanggan;
+        this.kelasParfum = kelasParfum;
+        this.notesParfum = notesParfum;
+        this.hargaCustom = hargaCustom;
+    }
+}
